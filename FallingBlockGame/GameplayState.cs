0e816a94a50e499e8cbc312f3bdbf5a74b06fcf3
NitroDino <edin.dz@hotmail.com>
2015-03-21 14:37:11 +0100

    Add gameLogic field to Gameplaystate

--- FallingBlockGame/GameplayState.cs
+++ FallingBlockGame/GameplayState.cs
@@ -9,12 +9,21 @@ namespace FallingBlockGame
 {
     public class GameplayState : IGameState
     {
+        private const int BLOCK_SIZE = 32;
+        
         private FallingBlockGame game;
         private FieldGameObjectBuilder fieldGameObjectBuilder;
+        private GameLogic gameLogic;
 
         public GameplayState(FallingBlockGame game)
         {
             this.game = game;
+            gameLogic = new GameLogic();
+
+            //fieldGameObjectBuilder = new FieldGameObjectBuilder(
+            //    gameLogic.Field,
+            //    BLOCK_SIZE,
+            //    blockTextureAtlas);
         }
         
         public void Update(GameTime gameTime)
