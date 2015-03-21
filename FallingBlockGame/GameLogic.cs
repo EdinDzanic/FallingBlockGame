76323b2788370d9a4b299de55bcb19d23d74a3b9
NitroDino <edin.dz@hotmail.com>
2015-03-21 14:32:02 +0100

    Add GameLogic class

---
+++ FallingBlockGame/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FallingBlockGame
+{
+    public class GameLogic
+    {
+        private const int FIELD_WIDTH = 10;
+        private const int FIELD_HEIGHT = 20;
+        
+        private Field field;
+        public Field Field { get { return field;} }
+
+        public GameLogic()
+        {
+            field = new Field(FIELD_HEIGHT, FIELD_WIDTH, 20, 20);
+        }
+    }
+}
