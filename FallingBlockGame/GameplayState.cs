f97ed7ff94b47e2e87be4236d5a9432162908efa
NitroDino <edin.dz@hotmail.com>
2015-03-21 14:27:17 +0100

    Add GameplayState class

---
+++ FallingBlockGame/GameplayState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using engine;
+
+namespace FallingBlockGame
+{
+    public class GameplayState : IGameState
+    {
+        private FallingBlockGame game;
+        private FieldGameObjectBuilder fieldGameObjectBuilder;
+
+        public GameplayState(FallingBlockGame game)
+        {
+            this.game = game;
+        }
+        
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
