2c0db5cf128c7053b7e6afe7b8132106eb9cb349
NitroDino <edin.dz@hotmail.com>
2015-03-23 09:45:25 +0100

    Add struct Coordinate

---
+++ FallingBlockGame/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FallingBlockGame
+{
+    internal struct Coordinate
+    {
+        public int X;
+        public int Y;
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
