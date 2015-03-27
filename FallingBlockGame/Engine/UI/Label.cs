139d0732ece55d704d292575b5d7c4e7ff3bd7b8
NitroDino <edin.dz@hotmail.com>
2015-03-27 13:08:27 +0100

    Add Label class

---
+++ FallingBlockGame/Engine/UI/Label.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace engine
+{
+    public class Label
+    {
+        public string Text { get; set; }
+        public Color TextColor { get; set; }
+        public SpriteFont Font { get; set; }
+
+        public Vector2 Position { get; set; }
+        public Vector2 Padding { get; set; }
+
+        public Color BackgroundColor { get; set; }
+        public Texture2D BackgroundImage { get; set; }
+
+        public Label(string text)
+        {
+            Text = text;
+            Padding = new Vector2(0, 0);
+            Position = new Vector2(0, 0);
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            graphics.SpriteBatch.DrawString(Font, Text, Position + Padding, TextColor);
+        }
+    }
+}
