16bb1c7828a8ff1559db983d203458ef7f388b35
NitroDino <edin.dz@hotmail.com>
2015-04-27 13:06:27 +0200

    Add SplashState class

---
+++ FallingBlockGame/SplashState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using engine;
+
+namespace FallingBlockGame
+{
+    public class SplashState : IGameState
+    {
+        private const int TIME = 3000;
+        private const string NEXT_STATE = "gameplay";
+
+        public bool IsActive { get; set; }
+        public Dictionary<string, IGameState> ChildStates { get; set; }
+        
+        private int timer;
+
+        private FallingBlockGame game;
+        private GameStateManager gameStateManager;
+
+        public SplashState(FallingBlockGame game, GameStateManager gameStateManager)
+        {
+            ChildStates = new Dictionary<string, IGameState>();
+            IsActive = false;
+
+            this.game = game;
+            this.gameStateManager = gameStateManager;
+
+            timer = TIME;
+        }
+        
+        public void Update(GameTime gameTime)
+        {
+            timer -= gameTime.ElapsedGameTime.Milliseconds;
+            
+            if (timer < 0)
+            {
+                gameStateManager.ChangeState(NEXT_STATE);
+            }
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            game.RenderManager.ClearScreen(Color.Black);
+        }
+    }
+}
