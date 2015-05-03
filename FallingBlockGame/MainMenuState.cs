2fae4f0fafc89c99bf00fb1abd967b54708214ff
NitroDino <edin.dz@hotmail.com>
2015-05-03 12:14:17 +0200

    Add skeleton for MainMenuState class

---
+++ FallingBlockGame/MainMenuState.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using engine;
+
+namespace FallingBlockGame
+{
+    public class MainMenuState : IGameState
+    {
+        public bool IsActive
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Dictionary<string, IGameState> ChildStates
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
