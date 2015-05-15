b5b7e64b9d8f319816cec94e5cbc1e67b808bc24
NitroDino <edin.dz@hotmail.com>
2015-05-15 13:28:42 +0200

    Refactor sound effect playing

--- FallingBlockGame/GameplayState.cs
+++ FallingBlockGame/GameplayState.cs
@@ -145,16 +145,7 @@ namespace FallingBlockGame
 
                     gameLogic.Update(move);
 
-                    if (gameLogic.IsDown)
-                    {
-                        blockFall.Play(1.0f, 0.0f, 0.0f);
-                        gameLogic.IsDown = false;
-                    }
-                    if (gameLogic.IsFullRow)
-                    {
-                        lineClear.Play(0.5f, 0.0f, 0.0f);
-                        gameLogic.IsFullRow = false;
-                    }
+                    PlaySoundEffects();
 
                     score.Text = string.Format("Score: {0}", gameLogic.Score);
                     nextShape.ImageTexture = nextShapes[gameLogic.NextShapeType];
@@ -171,6 +162,20 @@ namespace FallingBlockGame
                     gameLogic.Restart();
                     ChildStates["gameover"].IsActive = false;
                 }
+            }
+        }
+
+        private void PlaySoundEffects()
+        {
+            if (gameLogic.IsDown)
+            {
+                blockFall.Play(1.0f, 0.0f, 0.0f);
+                gameLogic.IsDown = false;
+            }
+            if (gameLogic.IsFullRow)
+            {
+                lineClear.Play(0.5f, 0.0f, 0.0f);
+                gameLogic.IsFullRow = false;
             }
         }
 
